92450a2b8eb9dba11b7dcdea69b18eb2237ac2eb
Aditya Pachupate <adityapachupate@gmail.com>
2025-07-24 11:45:42 +0530

    Update work experience and social media links; added new role as Programmer Analyst Trainee and updated Twitter handle

--- src/Constants/constant.js
+++ src/Constants/constant.js
@@ -1,10 +1,17 @@
 export const EXPERIENCES = [
   {
+    year: "July 2025 - Present",
+    role: "Programmer Analyst Trainee",
+    company: "Cognizant Technology solutions",
+    description: `Developing web applications using C#, .NET Core, MVC, and Web API. Working with SQL databases for efficient data management. Collaborating in Agile teams to build scalable applications.`,
+    technologies: [".Net Core", "SQL server", "Angular"],
+  },
+  {
     year: "Feb 2025 - Present",
     role: "Intern - Fullstack developer",
     company: "Cognizant Technology solutions",
-    description: `Developing web applications using C#, .NET Core, MVC, and Web API. Working with SQL databases for efficient data management. Collaborating in Agile teams to build scalable applications.`,
-    technologies: [".Net Core", "SQL server"],
+    description: `Worked collaboratively in a team to develop a full-stack e-commerce application using C#, .NET Core, MVC, Web API, and SQL databases.`,
+    technologies: [".Net Core", "SQL server", "Angular"],
   },
   {
     year: "July 2024 - June 2024",
@@ -13,5 +20,4 @@ export const EXPERIENCES = [
     description: `Worked on Intel MCS-51 microcontroller, developed embedded applications using C, and designed UI for system interfaces.`,
     technologies: ["C", "Proteus", "MCS-51", "Embedded systems"],
   },
-  
 ];
--- src/Constants/links.js
+++ src/Constants/links.js
@@ -1,7 +1,7 @@
 export const socialMediaLinks = {
     github: "https://github.com/AdityaPachupate",
     linkedin: "https://www.linkedin.com/feed/",
-    twitter: "https://x.com/ADITYAPACHUAPTE",
+    twitter: "https://x.com/btechguyy",
     instagram: "https://www.instagram.com/aditya_pachupate/",
     leetcode: "https://leetcode.com/u/adityapachupate/",
     gfg: "https://www.geeksforgeeks.org/user/adityapachupate/",
